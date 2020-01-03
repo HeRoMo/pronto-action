f2e335613b696b877223c55435bfb5b2a6846799
HeRoMo <hero@asterisk-works.jp>
2020-01-03 15:44:09 +0900

    modify entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-cd "$GITHUB_WORKSPACE"
+cd ${GITHUB_WORKSPACE}
 
+# Action Inputs
 export GITHUB_TOKEN=${INPUT_GITHUB_TOKEN}
+export RUNNERS=${INPUT_RUNNERS}
+export FORMATTERS=${FORMATTERS}
+export COMMIT=${INPUT_COMMIT}
 
-export PRONTO_PULL_REQUEST_ID="$(jq --raw-output .number "$GITHUB_EVENT_PATH")"
-export PRONTO_GITHUB_ACCESS_TOKEN="${GITHUB_TOKEN }"
-pronto run -r rubocop rails_best_practices brakeman -f github_status github_pr -c origin/master
+export PRONTO_PULL_REQUEST_ID="$(jq --raw-output .number '${GITHUB_EVENT_PATH}')"
+export PRONTO_GITHUB_ACCESS_TOKEN="${GITHUB_TOKEN}"
+bundle exec pronto run -r ${RUNNERS} -f ${FORMATTERS} -c ${COMMIT}
