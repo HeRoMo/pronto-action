079b21e95520b96e3fa08a51713b1a9e6a8b54da
HeRoMo <hero@asterisk-works.jp>
2020-01-03 17:03:10 +0900

    fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ cd ${GITHUB_WORKSPACE}
 # Action Inputs
 export GITHUB_TOKEN=${INPUT_GITHUB_TOKEN}
 export RUNNERS=${INPUT_RUNNERS}
-export FORMATTERS=${FORMATTERS}
+export FORMATTERS=${INPUT_FORMATTERS}
 export COMMIT=${INPUT_COMMIT}
 
 export PRONTO_PULL_REQUEST_ID="$(jq --raw-output .number '${GITHUB_EVENT_PATH}')"
 export PRONTO_GITHUB_ACCESS_TOKEN="${GITHUB_TOKEN}"
-bundle exec pronto run -r ${RUNNERS} -f ${FORMATTERS} -c ${COMMIT}
+bundle exec pronto run -r "${RUNNERS}" -f "${FORMATTERS}" -c ${COMMIT}
