46aacbed522e65aba641d606685c3eb448b9ce15
HeRoMo <hero@asterisk-works.jp>
2020-01-04 23:40:30 +0900

    fix bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ export RUNNERS=${INPUT_RUNNERS}
 export FORMATTERS=${INPUT_FORMATTERS}
 export COMMIT=${INPUT_COMMIT}
 
-export PRONTO_PULL_REQUEST_ID="$(jq --raw-output .number '${GITHUB_EVENT_PATH}')"
+export PRONTO_PULL_REQUEST_ID="$(jq --raw-output .number "${GITHUB_EVENT_PATH}")"
 export PRONTO_GITHUB_ACCESS_TOKEN="${GITHUB_TOKEN}"
 env
 ls -l
