30b947f4fb46a19d76c34b726c93905592a09b2e
HeRoMo <hero@asterisk-works.jp>
2020-01-05 10:07:59 +0900

    change pronto formatter

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ cd ${GITHUB_WORKSPACE}
 # Action Inputs
 export GITHUB_TOKEN=${INPUT_GITHUB_TOKEN}
 export RUNNERS=${INPUT_RUNNERS:-rubocop}
-export FORMATTERS=${INPUT_FORMATTERS:-github_pr}
+export FORMATTERS=${INPUT_FORMATTERS:-github_status github_pr}
 export COMMIT=${INPUT_COMMIT:-origin/master}
 
 export PRONTO_PULL_REQUEST_ID="$(jq --raw-output .number "${GITHUB_EVENT_PATH}")"
