d05431dea6701839e6ffc37e8eb6413bf3dd00c4
HeRoMo <hero@asterisk-works.jp>
2020-01-05 10:24:32 +0900

    output pronto result as exit code

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,3 +12,4 @@ export PRONTO_PULL_REQUEST_ID="$(jq --raw-output .number "${GITHUB_EVENT_PATH}")
 export PRONTO_GITHUB_ACCESS_TOKEN="${GITHUB_TOKEN}"
 echo "EXEC pronto run -r ${RUNNERS} -f ${FORMATTERS} -c ${COMMIT}"
 bundle exec pronto run -r "${RUNNERS}" -f "${FORMATTERS}" -c ${COMMIT}
+exit $?
