ec4c5a2bf5bda758bdec2cab93771ab40bc0b6c4
HeRoMo <hero@asterisk-works.jp>
2020-01-05 16:09:11 +0900

    changer input parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,13 @@ cd ${GITHUB_WORKSPACE}
 
 # Action Inputs
 export GITHUB_TOKEN=${INPUT_GITHUB_TOKEN}
-export RUNNERS=${INPUT_RUNNERS:-rubocop}
-export FORMATTERS=${INPUT_FORMATTERS:-github_status github_pr}
 export COMMIT=${INPUT_COMMIT:-origin/master}
+export RUNNER=${INPUT_RUNNER:-rubocop}
+export FORMATTERS=${INPUT_FORMATTERS:-github_status github_pr}
 
 export PRONTO_PULL_REQUEST_ID="$(jq --raw-output .number "${GITHUB_EVENT_PATH}")"
 export PRONTO_GITHUB_ACCESS_TOKEN="${GITHUB_TOKEN}"
-COMMAND="bundle exec pronto run -r ${RUNNERS} -f ${FORMATTERS} -c ${COMMIT}"
+COMMAND="bundle exec pronto run -c ${COMMIT} -r ${RUNNER} -f ${FORMATTERS}"
 echo "PRONTO_PULL_REQUEST_ID: ${PRONTO_PULL_REQUEST_ID}"
 echo "COMMAND: ${COMMAND}"
 $COMMAND
